72c11aafc373ed1b6c580e404560bee9ce7fd807
Łukasz Lassota <lukaszlassota@gmail.com>
2022-01-03 19:38:21 +0100

    Add sections loop to home page.

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,23 @@
 import React from 'react';
+import SectionsLoop from '../../components/SectionsLoop';
+import { HeroData } from '../../models/HeroData';
 
-export const Home = () => {
-  return <div>Home</div>;
+const SectionsData: HeroData[] = [
+  {
+    type: 'hero',
+    text: 'Excepteur sint occaecat cupidatat non proident sunt.',
+    img: 'https://i.ibb.co/G9bfTPH/breally-img.png',
+  },
+  // {
+  //   type: 'testimonial',
+  //   text: 'Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae.',
+  //   author: 'Peam Pim, The Actor',
+  // },
+  // {
+  //   type: 'newsletter',
+  // },
+];
+
+export const Home: React.FC = () => {
+  return <SectionsLoop sections={SectionsData} />;
 };
