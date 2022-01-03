fc3fc64ea071d4219783098ebf7d2318a04430d8
Łukasz Lassota <lukaszlassota@gmail.com>
2022-01-03 22:13:14 +0100

    Add data for newsletter section.

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import SectionsLoop from '../../components/SectionsLoop';
 import { HeroData } from '../../models/HeroData';
+import { NewsletterData } from '../../models/NewsletterData';
 import { TestimonialData } from '../../models/TestimonialData';
 
-const SectionsData: (HeroData | TestimonialData)[] = [
+const SectionsData: (HeroData | TestimonialData | NewsletterData)[] = [
   {
     type: 'hero',
     text: 'Excepteur sint occaecat cupidatat non proident sunt.',
@@ -14,9 +15,9 @@ const SectionsData: (HeroData | TestimonialData)[] = [
     text: 'Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae.',
     author: 'Peam Pim, The Actor',
   },
-  // {
-  //   type: 'newsletter',
-  // },
+  {
+    type: 'newsletter',
+  },
 ];
 
 export const Home: React.FC = () => {
