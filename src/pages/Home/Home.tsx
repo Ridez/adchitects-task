14dda73645bd7b99906458e45dcdabb0ad88e558
Łukasz Lassota <lukaszlassota@gmail.com>
2022-01-04 20:19:58 +0100

    Add loading page.

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,13 +4,23 @@ import { HeroData } from '../../models/HeroData';
 import { NewsletterData } from '../../models/NewsletterData';
 import { TestimonialData } from '../../models/TestimonialData';
 import { getPage } from '../../services/breallyApi';
+import './styles.scss';
 
 export const Home: React.FC = () => {
   const [sections, setSections] = useState<(HeroData | TestimonialData | NewsletterData)[]>([]);
+  const [isLoading, setIsloading] = useState<boolean>(true);
 
   useEffect(() => {
-    getPage('MPz3uDxgKR').then((res) => setSections(res.data.sections));
+    getPage('MPz3uDxgKR').then((res) => {
+      setSections(res.data.sections);
+      setIsloading(false);
+    });
   }, []);
 
-  return <SectionsLoop sections={sections} />;
+  return (
+    <>
+      <div className={`loading ${isLoading ? '' : 'loaded'}`}></div>
+      <SectionsLoop sections={sections} />
+    </>
+  );
 };
